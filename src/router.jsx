c5230e9275a5ede9c3d16fdd33581a5e4621111d
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-26 11:39:01 +0200

    feat(page): add ProfilePage.jsx with style and absolute paths for img/icons

--- src/router.jsx
+++ src/router.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './ui/styles/main.css';
 // import HomePage from './ux/pages/HomePage';
-import LoginPage from './ux/pages/LoginPage';
+// import LoginPage from './ux/pages/LoginPage';
+import ProfilePage from './ux/pages/ProfilePage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage />
+    <ProfilePage />
   </StrictMode>,
 )
