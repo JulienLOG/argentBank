9c920a766261434d4b61299a7473016840c12d5f
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-26 20:59:12 +0200

    refactor(Header): add useMatch and debug of Header.jsx component

--- src/ux/components/Header.jsx
+++ src/ux/components/Header.jsx
@@ -1,20 +1,39 @@
-export default function Header() {
+// _libs
+import { useMatch, Link } from "react-router";
+
+export default function Header({ name = "Tony" }) {
+	const routePaths = { index: "/", login: "/login", profile: "/profile" };
+	const matchProfilePath = useMatch(routePaths.profile);
+
 	return (
 		<nav className="main-nav">
-			<a className="main-nav-logo" href="./index.html">
+			<Link className="main-nav-logo" to={routePaths.index}>
 				<img
 					className="main-nav-logo-image"
 					src="/src/ui/images/argentBankLogo.png"
 					alt="Argent Bank Logo"
 				/>
 				<h1 className="sr-only">Argent Bank</h1>
-			</a>
-			<div>
-				<a className="main-nav-item" href="./sign-in.html">
-					<i className="fa fa-user-circle"></i>
-					Sign In
-				</a>
-			</div>
+			</Link>
+			{matchProfilePath ? (
+				<div>
+					<Link className="main-nav-item" to={routePaths.profile}>
+						<i className="fa fa-user-circle"></i>
+						{name}
+					</Link>
+					<Link className="main-nav-item" to={routePaths.index}>
+						<i className="fa fa-sign-out"></i>
+						Sign Out
+					</Link>
+				</div>
+			) : (
+				<div>
+					<Link className="main-nav-item" to={routePaths.login}>
+						<i className="fa fa-user-circle"></i>
+						Sign In
+					</Link>
+				</div>
+			)}
 		</nav>
 	);
 }
