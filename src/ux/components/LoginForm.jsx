5f297f4b958b9d541bd61e4b9ea0a3b756b8f6b5
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-29 14:48:32 +0200

    refactor(LoginForm): refactor LoginForm component with states controller

--- src/ux/components/LoginForm.jsx
+++ src/ux/components/LoginForm.jsx
@@ -1,34 +1,39 @@
 // _libs
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const inputs = {
 	email: { id: "userEmail", name: "email", value: null },
 	password: { id: "userPassword", name: "password", value: null },
 };
 
-const loginFormat = { [inputs.email.name]: null, [inputs.password.name]: null };
+const inputs_login = { [inputs.email.name]: "", [inputs.password.name]: "" };
+const inputs_controller = { [inputs.email.name]: "", [inputs.password.name]: "",  msgErr : false};
+
 
 export default function LoginForm() {
-	const [user, setUser] = useState({ ...loginFormat });
-	const userRef = useRef({ email: "", password: "" });
+	console.log("render to start");
+	const [userLogin, setUserLogin] = useState({ ...inputs_login });
+	const [loginController, setLoginController] = useState({ ...inputs_controller });
 
-	const handleChange = (e) => {
-		const name = e.target.name;
-		userRef.current[name] = e.target.value;
+	const handleChange = (evt) => {
+		const {name ,value} = evt.target;
+		setLoginController((prev) => ({ ...prev, [name]: value }));
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		setUser((itm) => ({
-			...itm,
-			[inputs.email.name]: userRef.current.email,
-			[inputs.password.name]: userRef.current.password,
-		}));
-		resetForm(event);
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		if (!loginController.email.length || !loginController.password.length){
+			setLoginController(prev => ({...prev, msgErr: true}));
+			return;
+		} else {
+			setLoginController(prev => ({...prev, msgErr: false}));
+		}
+		setUserLogin(prev => ({ ...prev, email: loginController.email, password: loginController.password}));
+		handleClear();
 	};
 
-	const resetForm = (event) => {
-		event.currentTarget.reset();
+	const handleClear = () => {
+		setLoginController(inputs_controller);
 	};
 
 	return (
@@ -39,6 +44,7 @@ export default function LoginForm() {
 					type="text"
 					id={inputs.email.id}
 					name={inputs.email.name}
+					value={loginController.email}
 					onChange={handleChange}
 				/>
 			</div>
@@ -48,6 +54,7 @@ export default function LoginForm() {
 					type="password"
 					id={inputs.password.id}
 					name={inputs.password.name}
+					value={loginController.password}
 					onChange={handleChange}
 				/>
 			</div>
@@ -55,6 +62,7 @@ export default function LoginForm() {
 				<input type="checkbox" id="remember-me" />
 				<label htmlFor="remember-me">Remember me</label>
 			</div>
+			{ loginController.msgErr && <p>You must enter your email address and password to log in.</p>}
 			<button className="sign-in-button" type="submit">
 				Sign In
 			</button>
