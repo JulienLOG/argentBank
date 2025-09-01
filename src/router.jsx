d11df1d19502ad1d0244dc2815265d4a1920b5b1
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-09-01 22:23:56 +0200

    feat(store): add of a Provider component from react-redux in the router.jsx file

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { routes } from "./routes.jsx";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
 // _ui
 import "./ui/styles/main.css";
 import { AuthProvider } from "./store/ContextAuth.jsx";
@@ -10,8 +12,10 @@ import { AuthProvider } from "./store/ContextAuth.jsx";
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
 	<StrictMode>
-		<AuthProvider>
-			<RouterProvider router={routes} />
-		</AuthProvider>
+		<Provider store={store}>
+			<AuthProvider>
+				<RouterProvider router={routes} />
+			</AuthProvider>
+		</Provider>
 	</StrictMode>
 );
